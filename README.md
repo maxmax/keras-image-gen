b1f8434a12d6ebfb567d61136e5c0c85ae5fb383
maxmax <maxbakaiev@gmail.com>
2023-09-01 21:35:12 +0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# keras-image-gen
- 
+## keras-image-gen
+
+`pip install -r requirements.txt`
+
+`python3 app.py`
+
+In case of problems related to import keras_cv, use this solution:
+
+`git clone https://github.com/keras-team/keras-cv.git`
+
+`cd keras-cv`
+
+`python setup.py install`
+
+### Example result
--- app.py
+++ app.py
@@ -3,4 +3,17 @@ import keras_cv
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
-print('hello')
+model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+
+
+images = model.text_to_image("photograph of an black cat riding a unicorn", batch_size=3)
+
+def plot_images(images):
+    plt.figure(figsize=(20, 20))
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i + 1)
+        plt.imshow(images[i])
+        plt.axis("off")
+
+
+plot_images(images)
